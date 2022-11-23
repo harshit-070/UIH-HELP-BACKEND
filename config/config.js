2f0ad82c33e5cc78b9bdc617b8683ccce62c92b5
Harshit <harshitmitta070@gmail.com>
2022-11-23 14:48:55 +0530

    UIA

---
+++ config/config.js
@@ -0,0 +1,23 @@
+// sendgrid key
+exports.Sendgrid = process.env.SENDGRID_KEY;
+
+// mongo database
+exports.mongo = process.env.MONGO_DATABASE;
+
+// jwt authentication
+exports.accessToken = process.env.ACCESS_TOKEN_SECRET;
+exports.refereshToken = process.env.REFRESH_TOKEN_SECRET;
+
+exports.accessTokenLife = process.env.ACCESS_TOKEN_LIFE;
+exports.refereshTokenLife = process.env.REFRESH_TOKEN_LIFE;
+
+//Redis
+exports.redisHost = process.env.REDIS_HOST;
+exports.redisPassword = process.env.REDIS_PASSWORD;
+exports.redisPort = process.env.REDIS_PORT;
+
+// google auth
+exports.googleAuth = process.env.OAuth2Client;
+exports.googlePublicKey = process.env.Google_Public_Key;
+//stripe payment
+exports.stripePayment = process.env.STRIPE_SECRET_TOKEN;
